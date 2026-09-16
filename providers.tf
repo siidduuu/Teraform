11a1c5365c299ec46eec96f96f345c0307346a10
Siddu <siidduu95@gmail.com>
2026-09-16 15:29:48 +0530

    git hub actions workflow

--- providers.tf
+++ providers.tf
@@ -1,15 +1,12 @@
 terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      # This allows any 5.x version starting from 5.80, but explicitly skips the broken 5.100.0
-      version = ">= 5.80, != 5.100.0"
-    }
+  backend "s3" {
+    bucket       = "terraform-assignment-state-063392961907"
+    key          = "assignment/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
   }
 }
 
 provider "aws" {
   region = "us-east-1"
 }
-
